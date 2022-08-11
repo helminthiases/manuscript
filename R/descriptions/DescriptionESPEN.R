9c07537521dcc92a9a9b0856faecd0517dca0bf7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-11 23:18:48 +0100

    Add R/descriptions/DescriptionESPEN.R

---
+++ R/descriptions/DescriptionESPEN.R
@@ -0,0 +1,26 @@
+# Title     : DescriptionESPEN.R
+# Objective : Description ESPEN
+# Created by: greyhypotheses
+# Created on: 11/08/2022
+
+
+DescriptionESPEN <- function () {
+
+  terms <- c('georeliability', 'longitude, latitude', 'year',
+             'hk\\underline{\\hspace{0.125cm}}examined, hk\\underline{\\hspace{0.125cm}}positive',
+             'asc\\underline{\\hspace{0.125cm}}examined, asc\\underline{\\hspace{0.125cm}}positive',
+             'tt\\underline{\\hspace{0.125cm}}examined, tt\\underline{\\hspace{0.125cm}}positive')
+
+  descriptions <- c(
+    "The reliability of an observation's geographic variables.",
+    "A site's longitude and latitude co$\\ddot{o}$rdinates.",
+    "The observation's year.",
+    "The number of individuals examined, and the number testing positive, respectively.$^{1}$.",
+    "$ditto^{2}$",
+    "$ditto^{3}$" )
+
+  frame <- data.frame(term = terms, description = descriptions)
+
+  return(frame)
+
+}
