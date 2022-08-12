9cd5230ee1c59b24efb2e6b3e264710a423c1596
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-12 02:08:30 +0100

    Update R/descriptions/DescriptionAlgorithms.R

--- R/descriptions/DescriptionAlgorithms.R
+++ R/descriptions/DescriptionAlgorithms.R
@@ -9,12 +9,12 @@ DescriptionAlgorithms <- function () {
   terms <- c('$i, n$', '$x_{i}$', '$\\mathbf{\\delta}(x_{i})$', '$\\mathbf{\\beta}$', '$Y_{i}$', '$p_{i}$')
 
   descriptions <- c(
-    '$i$ denotes the $i^{th}$ observation. $i = 1, \\ldots, n$, i.e., $n$ is the number of observations',
+    '$i$ denotes the $i^{th}$ observation, and $n$ is the number of observations.  Hence, $i = 1, \\ldots, n$.',
     'The location of the $i^{th}$ observation',
     'A vector of terms that depend on zero or more independent variables at location $x_{i}$',
     'The vector of regression coefficients vis-$\\grave{a}$-vis $\\mathbf{\\delta}(x_{i})$',
     'The outcome at location $x_{i}$',
-    'The prevalence, i.e., $\\# of positives_{i}/\\# of examinations_{i}$, at location $x_{i}$')
+    'The prevalence $\\nu_{i}/e_{i}$ wherein $\\nu$ is the $\\#$ of positives, and $e_{i}$ is the $\\#$ of examinations, at location $x_{i}$')
 
   frame <- data.frame(term = terms, description = descriptions)
 
