02bc2179a3a1902bbc0a3bf183add0e43421928e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-21 02:47:01 +0100

    Update R/descriptions/DescriptionSpecies.R

--- R/descriptions/DescriptionSpecies.R
+++ R/descriptions/DescriptionSpecies.R
@@ -6,10 +6,10 @@
 DescriptionSpecies <- function() {
 
   # The diseases
-  field <- c('species', 'abbreviation', 'name', 'adult inhabits', 'infective stage', '\\makecell[l]{infective organism, lives in}')
-  ascariasis <- c('Ascaris lumbricoides', 'A. lumbricoides', 'Roundworm', 'small intestine', 'ova', 'environment')
-  trichuriasis <- c('Trichuris trichiura', 'T. trichiura', 'Whipworm', 'caecum, colon', 'ova', 'environment')
-  hookworm <- c('\\makecell[l]{Ancylostoma duodenale, Necator americanus}', 'A. duodenale, N. americanus', 'Hookworm', 'upper small intestine', 'larvae', 'environment (soil)')
+  field <- c('species', 'abbr.', 'name', '\\makecell[l]{adult \\\\ inhabits}', '\\makecell[l]{infective \\\\ stage}')
+  ascariasis <- c('\\makecell[l]{Ascaris \\\\ lumbricoides}', '\\makecell[l]{A.\\\\ lumbricoides}', 'Roundworm', '\\makecell[l]{small \\\\ intestine}', 'ova')
+  trichuriasis <- c('\\makecell[l]{Trichuris \\\\ trichiura}', '\\makecell[l]{T.\\\\ trichiura}', 'Whipworm', '\\makecell[l]{caecum, \\\\ colon}', 'ova')
+  hookworm <- c('\\makecell[l]{Ancylostoma\\\\ duodenale,\\\\ Necator\\\\ americanus}', 'A. duodenale, N. americanus', 'Hookworm', 'upper small intestine', 'larvae')
 
   frame <- data.frame(field = field, ascariasis = ascariasis, trichuriasis = trichuriasis, hookworm.disease = hookworm)
 
