63188aeff366688320f3198b50741832727049ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-21 04:43:53 +0100

    Update R/metrics/MetricsSpecies.R

--- R/metrics/MetricsSpecies.R
+++ R/metrics/MetricsSpecies.R
@@ -3,20 +3,20 @@
 # Created by: greyhypotheses
 # Created on: 21/08/2022
 
-MetricsSpecies <- function () {
+MetricsSpecies <- function (pathstr) {
 
   # Disease names, labels
   diseases <- c('Ascariasis', 'Trichuriasis', 'Hookworm disease')
-  labels <- c('Ascariasis', 'Trichuriasis', '\\makecell[l]{Hookworm, disease}')
+  labels <- c('Ascariasis', 'Trichuriasis', '\\makecell[l]{Hookworm \\\\ disease}')
   names <- data.frame(disease = diseases, labels = labels)
 
   # The disability ajusted life years per 100,000 inhabitants
-  daly <- read.csv(file = 'data/daly_level_4.csv') %>%
+  daly <- read.csv(file = file.path(pathstr, 'data', 'daly_level_4.csv')) %>%
     dplyr::filter(disease %in% diseases)
   daly <- dplyr::rename(daly, daly.lower = lower, daly.upper = upper)
 
   # The prevalence per 1000,000 inhabitants
-  prevalence <- read.csv(file = 'data/prev_level_4.csv') %>%
+  prevalence <- read.csv(file = file.path(pathstr, 'data', 'prev_level_4.csv')) %>%
     dplyr::filter(disease %in% diseases)
   prevalence <- dplyr::rename(prevalence, prevalence.lower = lower, prevalence.upper = upper)
 
