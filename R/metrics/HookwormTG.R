6da5adf62eacfe656c4fcfd512cb007574bc7a50
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-08-31 23:46:21 +0100

    Update R/metrics/HookwormTG.R

--- R/metrics/HookwormTG.R
+++ R/metrics/HookwormTG.R
@@ -7,8 +7,8 @@ HookwormTG <- function (pathstr) {
 
   # We'll use the full names in the manuscript
   names <- data.frame(metric = c('daly_per_100k', 'prev_cases_per_100k'),
-                      label = c('\\makecell[l]{DALY \\\\ per 100k}',
-                                '\\makecell[l]{Prevalence \\\\ per 100k}'))
+                      label = c('DALY/100k',
+                                'prev./100k'))
 
 
   # The neglected tropical diseases data
